f6df633a03a9383379d1c2eca2103dab7e828c62
JohnDaise <jdaise@gmail.com>
2018-10-15 13:51:49 -0400

    basic cards

---
+++ src/components/UserCard.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Grid, Image, Card, Header } from 'semantic-ui-react'
+
+class UserCard extends React.Component {
+
+
+
+
+
+render(){
+  console.log(this.props)
+  return(
+    <Grid.Column>
+    <Card >
+        // <Image src={this.props.user.pic_url} size='medium' rounded={true} centered verticalAlign='middle' />
+      <Card.Content>
+        <Card.Header as='h3'>{this.props.user.name}</Card.Header>
+        Email: {this.props.user.email}<br/>
+        Phone Number: {this.props.user.phone_number}<br/>
+        Bio: {this.props.user.bio}
+      </Card.Content>
+
+    </Card>
+    </Grid.Column>
+  )
+  }
+}
+
+export default UserCard;
+
+//showDetail for a single instrument
+// <Card >
+//   <Card.Header as='h3'>{this.props.instrument.brand} {this.props.instrument.name}</Card.Header>
+//     {this.props.instrument.category_id === 2 ?
+//     <Image src={this.props.instrument.pic_url} size='medium' rounded={true} centered verticalAlign='middle' />
+//     :<Image src={this.props.instrument.pic_url} size='small' rounded={true} centered verticalAlign='middle' />}
+//   <Card.Content>
+//     <Header as='h5' >
+//       Color: {this.props.instrument.color} <br/>
+//       Condition: {this.props.instrument.condition}<br/>
+//     </Header>
+//     <Header as='h4' color='red' >
+//       Price: ${this.props.instrument.price}<br/>
+//     </Header>
+//   </Card.Content>
+// </Card>
--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import UserCard from './UserCard'
+
+import { Grid } from 'semantic-ui-react'
+
 
 
 class UsersList extends React.Component {
@@ -8,9 +12,14 @@ class UsersList extends React.Component {
   render(){
     return (
       <React.Fragment>
+      <Grid centered columns={3} >
       {this.props.allUsers.map(user =>
-        <h1> {user.name} </h1>
-      )}
+        <UserCard
+            key={user.id}
+            user={user}
+            /> )
+      }
+      </Grid>
       </React.Fragment>
     );
     }
