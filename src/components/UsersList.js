eda6162af1675bc0805aad268118cea4eddcc29d
JohnDaise <jdaise@gmail.com>
2018-10-16 16:44:18 -0400

    spinner works on load

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -3,15 +3,26 @@ import React from "react";
 import UserCard from './UserCard'
 import { connect } from "react-redux";
 
-import { Grid } from 'semantic-ui-react'
+import { Grid, Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
 
 
 const UsersList = props => (
+  <React.Fragment>
+     {props.loading ?
+        <Segment>
+        <Dimmer active>
+          <Loader size='huge'>Loading</Loader>
+        </Dimmer>
+        <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+        <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+        <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+      </Segment> : null }
   <Grid centered columns={3} >
     {props.users.map(user => (
       <UserCard user={user} />
     ))}
   </Grid>
+  </React.Fragment>
 );
 
 
